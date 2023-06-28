8050ec9eaa2b59170968e5f1aab11699c2dcdb43
ddcarmos <daniele1b@hotmail.com>
2023-06-28 18:51:30 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Informe um numero: ");
+    int n = int.Parse(Console.ReadLine());
+    
+    int f = fatorial(n);
+    Console.WriteLine(f);
+    
+  }
+  public static int fatorial(int numero)
+   {
+    int f = 1;
+    for (int i=1;i<=numero;i++)
+    {
+      f= f * i;
+    }
+    return f;
+   }
+}
+
